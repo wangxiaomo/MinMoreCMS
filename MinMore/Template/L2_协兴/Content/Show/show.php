a09b28d80265cbfd796e67c87548405a59c945d3
wangxiaomo <wxm4ever@gmail.com>
2016-06-25 17:25:43 +0800

    add template

---
+++ MinMore/Template/L2_协兴/Content/Show/show.php
@@ -0,0 +1,69 @@
+<?php if (!defined('MINMORE_VERSION')) exit(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="favicon.ico" rel="shortcut icon" />
+<link rel="canonical" href="{$config_siteurl}" />
+<title><if condition=" isset($SEO['title']) && !empty($SEO['title']) ">{$SEO['title']}</if>{$SEO['site_title']}</title>
+<meta name="description" content="{$SEO['description']}" />
+<meta name="keywords" content="{$SEO['keyword']}" />
+<link href="{$config_siteurl}statics/themes/L2_协兴/css/common.css" rel="stylesheet" type="text/css" />
+<link href="{$config_siteurl}statics/themes/L2_协兴/css/details.css" rel="stylesheet" type="text/css" />
+<script src="{$config_siteurl}statics/js/jquery.js"></script>
+</head>
+
+<body>
+<template file="Content/Mods/header.php" />
+<div class="content_box">
+<div class="content_up">
+	<div class="up_left float_left">
+    	<div class="left_title">
+    		<span>{:getCategory($catid, 'catname')}</span>
+        </div>
+        <div class="up_left_cont">
+        	<h3>{$title}</h3>
+            <p class="details_time"><span class="float_left">来源：{$copyfrom|default='本站原创'}</span><span class="float_right">发布时间：{$updatetime|strtotime=###|date='Y-m-d',###}</span></p>
+            <div class="details_text">
+                <if condition="$catid eq 7">
+                    <div style="text-align:center">
+                        <iframe src="{$description}" allowfullscreen="" class="embed-responsive-item" frameborder="0" height=498 width=510></iframe>
+                    </div>
+                <else />
+                    {$content}
+                </if>
+                    <div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more">分享到：</a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间">QQ空间</a><a href="#" class="bds_sqq" data-cmd="sqq" title="分享到QQ好友">QQ好友</a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信">微信</a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博">新浪微博</a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网">人人网</a><a href="#" class="bds_douban" data-cmd="douban" title="分享到豆瓣网">豆瓣网</a></div>
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"share":{"bdSize":16}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+            </div>
+        </div>
+    </div>
+    <div class="up_right float_right">
+        <div class="gonggao">
+        	<div class="gg_title"><img class="float_left" src="{$config_siteurl}statics/themes/L2_协兴/images/title_4.png" /><a class="float_right" href="#">更多</a></div>
+            <div class="gg_cont">
+            	<ul>
+                    <content action="lists" catid="2" order="id DESC" num="3">
+                        <volist name="data" id="vo">
+                            <li><img src="{$config_siteurl}statics/themes/L2_协兴/images/icon/num_1.png" /><a href="#">省内煤市要闻回顾</a></li>
+                        </volist>
+                    </content>
+                </ul>
+            </div>
+        </div>
+        <div class="enter">
+            <ul>
+                <li><img src="{$config_siteurl}statics/themes/L2_协兴/images/enter_1.png" /></li>
+                <li><img src="{$config_siteurl}statics/themes/L2_协兴/images/enter_2.png" /></li>
+                <li><img src="{$config_siteurl}statics/themes/L2_协兴/images/enter_3.png" /></li>
+                <li><img src="{$config_siteurl}statics/themes/L2_协兴/images/enter_4.png" /></li>
+                <li><img src="{$config_siteurl}statics/themes/L2_协兴/images/enter_5.png" /></li>
+                <li><img src="{$config_siteurl}statics/themes/L2_协兴/images/enter_6.png" /></li>
+                <li style="margin:0;"><img src="{$config_siteurl}statics/themes/L2_协兴/images/enter_7.png" /></li>
+            </ul>
+        </div>
+    </div>
+</div>
+<template file="Content/Mods/footer.php" />
+</div>
+</body>
+</html>
---
+++ MinMore/Template/L2_经开/Content/Mods/footer.php
@@ -0,0 +1,61 @@
+<div class="fenzhen">
+	<span class="float_left"><img src="{$config_siteurl}statics/themes/L2_经开/images/title_3.png" /></span>
+    <p class="float_left"><a href="#">广安区</a><a href="#">前锋区</a><a href="#">华蓥市</a><a href="#">邻水县</a><a href="#">岳池县</a><a href="#">武胜县</a></p>
+</div>
+<div class="link">
+	<span>友情链接</span>
+    <div class="link_select">
+    <form>
+    	<select>
+        	<option>选择局</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+        </select>
+        <select>
+        	<option>选择派出所</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+        </select>
+        <select>
+        	<option>选择警务室</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+        </select>
+        <button>进 入</button>
+    </form>
+    </div>
+</div>
+<div class="link_img">
+	<a href="#"><img src="{$config_siteurl}statics/themes/L2_经开/images/img/link_1.jpg" /></a>
+    <a href="#"><img src="{$config_siteurl}statics/themes/L2_经开/images/img/link_2.jpg" /></a>
+    <a href="#"><img src="{$config_siteurl}statics/themes/L2_经开/images/img/link_3.jpg" /></a>
+    <a href="#"><img src="{$config_siteurl}statics/themes/L2_经开/images/img/link_4.jpg" /></a>
+</div>
+<div class="foot_search_box">
+	<div class="foot_search">
+    <form>
+        <select class="float_left" style="margin-left:30px;">
+            <option>政府网站</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+        </select>
+        <select class="float_left" style="margin-left:240px;">
+        	<option>政法网站</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+        </select>
+        <select class="float_right" style="margin-right:30px;">
+        	<option>推荐网站</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+            <option>广安区公安分局</option>
+        </select>
+    </form>
+    </div>
+</div>
+<div class="footer">广安市公安局广安区分局 版权所有</div>
