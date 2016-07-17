27cb1f99005229d08f63ed271cc8064cb81b315d
wangxiaomo <wxm4ever@gmail.com>
2016-07-17 14:00:52 +0800

    update register and login url

--- MinMore/Common/Conf/minmore.php
+++ MinMore/Common/Conf/minmore.php
@@ -12,10 +12,10 @@ return array(
     "GLOBAL_SITE_DOMAIN" => "l1.minmore.com",
     "SITE_ROLE_PARENT" => 2,
     "MINMORE_CACHE_PREFIX"  =>  "cache:minmore:",
-    "WORK_BUILDING_URL" =>  "http://125.66.2.25:28082/wsga",
+    "WORK_BUILDING_URL" =>  "http://125.66.2.25:28081/wsga/wsga/web/index.htm",
     "VCODE_SMS_TEMPLATE" => "请输入{vcode}进行本次验证。",
     "VCODE_LENGTH"  =>  6,
     "HACK_SMS_VCODE"    =>  "345211",
-    "MINMORE_REGISTER_URL"  =>  "http://125.66.2.25:28082/wsga/user/register.html",
-    "MINMORE_LOGIN_URL"     =>  "http://125.66.2.25:28082/wsga/user/login.html",
+    "MINMORE_REGISTER_URL"  =>  "http://s1.minmore.com:28081/wsga/wsga/web/personal-center/user-regist.htm",
+    "MINMORE_LOGIN_URL"     =>  "http://s1.minmore.com:28081/wsga/wsga/web/personal-center/user-login.htm",
 );
--- MinMore/Template/L1_Global/Content/Mods/QR.php
+++ MinMore/Template/L1_Global/Content/Mods/QR.php
@@ -1,5 +1,6 @@
 <!-- 二维码侧边栏-->
 <div class="erweima">
+    <a href="#"><img src="{$config_siteurl}statics/themes/L1_Global/images/cebianlan_weibo.png" alt=""/></a>
     <a href="#"><img src="{$config_siteurl}statics/themes/L1_Global/images/cebianlan_police.png" alt=""/></a>
     <a href="#"><img src="{$config_siteurl}statics/themes/L1_Global/images/cebianlan_phone.png" alt=""/></a>
     <a href="#"><img src="{$config_siteurl}statics/themes/L1_Global/images/cebianlan-weibo.png" alt="" style="margin:10px 0 0 25px"/></a>
