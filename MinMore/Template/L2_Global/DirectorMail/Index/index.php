acdbdb3cb8fc6c129598e878de0ccfca6255e98f
Billow <drower0@gmail.com>
2016-08-03 17:54:41 +0800

    add L2_Global/DirectorMail/Index

---
+++ MinMore/Template/L2_Global/DirectorMail/Index/index.php
@@ -0,0 +1,90 @@
+<?php if (!defined('MINMORE_VERSION')) exit(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="favicon.ico" rel="shortcut icon" />
+        <link rel="canonical" href="{$config_siteurl}" />
+        <title><if condition=" isset($SEO['title']) && !empty($SEO['title']) ">{$SEO['title']}</if>{$SEO['site_title']}</title>
+        <meta name="description" content="{$SEO['description']}" />
+        <meta name="keywords" content="{$SEO['keyword']}" />
+        <link href="{$config_siteurl}statics/themes/L2_Global/css/common.css" rel="stylesheet" type="text/css" />
+        <link href="{$config_siteurl}statics/themes/L2_Global/css/list.css" rel="stylesheet" type="text/css" />
+        <link href="{$config_siteurl}statics/extres/directormail/css/mailbox.css" rel="stylesheet" type="text/css" />
+        <script src="{$config_siteurl}statics/js/jquery.js"></script>
+    </head>
+    <body>
+        <template file="Content/Mods/header.php" />
+        <div class="content_box">
+            <div class="content_up">
+                <div class="up_left float_left">
+		<div clkss="content-fd">
+		  <div class="mailbox-list">
+		    <div class="content-hd">
+		      局长信箱－最新回复信件
+		    </div>
+		    <div class="mailbox-data-list">
+		      <table>
+			<thead>
+			  <tr>
+			    <th>来信主题</th>
+			    <th>状态</th>
+			    <th>时间</th>
+			    <th>受理单位</th>
+			  </tr>
+			</thead>
+			<tbody>
+			    <volist name="dataList" id="vo">
+			    <tr>
+			      <td><a href="{:U('mail?mailid='.$vo['id'])}">{$vo.zhuti}</a></td>
+			      <if condition=" $vo['reply'] ">
+			      <td>已回复</td>
+			      <else /><td>未回复</td>
+			      </if>
+			      <td>{$vo.createtime|date="Y-m-d H:i:s",###}</td>
+			      <td>{$vo.roleid}</td>
+			    </tr>
+			    </volist>
+			</tbody>
+		      </table>
+		    </div>
+		  </div>
+		</div>
+                </div>
+                <div class="up_right float_right">
+                    <div class="gonggao">
+                        <div class="gg_title"><img class="float_left" src="{$config_siteurl}statics/themes/L2_Global/images/title_4.png" /><a class="float_right" href="{:getCategory(60, 'url')}">更多</a></div>
+                        <div class="gg_cont">
+                            <ul>
+                                <content action="lists" catid="60" order="id DESC" num="3">
+                                    <volist name="data" id="vo">
+                                        <li><img src="{$config_siteurl}statics/themes/L2_Global/images/icon/num_{$i}.png" /><a href="{$vo.url}">{$vo.title|str_cut=###,12}</a></li>
+                                    </volist>
+                                </content>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="enter">
+                        <ul>
+                            <li><a href="{:U('Content/Index/lists', array('catid'=>45))}"><img src="{$config_siteurl}statics/themes/L2_Global/images/enter_1.png" /></a></li>
+                            <li><a href="http://weibo.com/u/2918589882?nick=%E9%87%91%E7%9B%BE%E5%B9%BF%E5%AE%89&is_hot=1"><img src="{$config_siteurl}statics/themes/L2_Global/images/enter_2.png" /></a></li>
+                            <li><a href="{:U('DirectorMail/Consult/add@' . C('GLOBAL_SITE_DOMAIN'), array('type'=>'wsjb'))}"><img src="{$config_siteurl}statics/themes/L2_Global/images/enter_3.png" /></a></li>
+                            <li><a href="{:U('Content/Site/l2_service')}"><img src="{$config_siteurl}statics/themes/L2_Global/images/enter_4.png" /></a></li>
+                            <li><img src="{$config_siteurl}statics/themes/L2_Global/images/enter_5.png" /></li>
+                            <li><a href="{:U('DirectorMail/Index/add@' . C('GLOBAL_SITE_DOMAIN'))}"><img src="{$config_siteurl}statics/themes/L2_Global/images/enter_6.png" /></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        $(function () {
+            $(".query-button").on("click", function (e) {
+                e.preventDefault();
+                window.location = "{:U('DirectorMail/Index/search')}";
+            });
+        });
+    </script>
+</body>
+<template file="Content/Mods/footer.php" />
