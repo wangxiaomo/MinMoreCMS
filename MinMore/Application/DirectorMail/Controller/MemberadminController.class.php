cfbc69b7ef370b285747451fc26cb8ba71d04af6
Billow <drower0@gmail.com>
2016-09-04 16:57:25 +0800

    add feature: forward of membermail

--- MinMore/Application/DirectorMail/Controller/MemberadminController.class.php
+++ MinMore/Application/DirectorMail/Controller/MemberadminController.class.php
@@ -19,7 +19,7 @@ class MemberadminController extends AdminBase {
         $status= I('get.status');
         $query= I('post.keyword');
         $where = array(
-                'roleid' => get_site_role(),
+		'deptid'=>get_department_id()
             );
         $type = I('get.type');
         if ($type != '全部' && !empty($type)) {
@@ -67,6 +67,16 @@ class MemberadminController extends AdminBase {
         } else {
             $id = I('get.id', 0, 'intval');
             $info = $this->db->where(array('id' => $id))->find();
+	    if($info['deptid']!=get_department_id()){
+		$this->error("您无权查看该信件");
+		}
+	    $comments=M('comment')->where(array('mailid'=>$id,'mailtype'=>2))->order(array('createtime'=>asc))->select();
+	C('DB_PREFIX',"");
+	    foreach($comments as &$cmt){
+			$cmt['from']=M("huoyi_office")->where(array('id'=>$cmd['from']))->getField('oname');
+			//$cmt['to']=M("huoyi_office")->where(array('id'=>$cmd['to']))->getField('oname');
+		}
+	C('DB_PREFIX',"minmore_");
             $vo = M('MembermailUser')->where(array('uid'=>$info['uid']))->find();
             $info['usermsg'] = $vo['tel'].$vo['remarks'];
             $quickreply = M('DirectormailQuickreply')->where(array('roleid'=>get_admin_role()))->getField('quickreply', true);
@@ -75,6 +85,9 @@ class MemberadminController extends AdminBase {
             }
             if (!empty($quickreply)) {
                 $this->assign('quickreply', $quickreply);    
+            }
+            if (!empty($comments)) {
+                $this->assign('comments', $comments);    
             }
             $this->assign('info', $info);
             $this->display();
@@ -157,5 +170,75 @@ class MemberadminController extends AdminBase {
             $error = $this->db->getError();
             $this->error($error ? $error : '删除失败！');
         }
+    }
+
+    //获取可转发的下级部门
+    public function get_sub_department(){
+	    $dept_id=get_department_id();
+	    C('DB_PREFIX',"");
+	    $M_office=M('huoyi_office');
+	    C('DB_PREFIX',"minmore_");
+	    $level=$M_office->where(array('oid'=>$dept_id))->getField('olevel');
+	    $level=intval($level);
+	    $where=array('oheadid'=>$dept_id,'olevel'=>intval($level)+1);
+	    switch($level){
+		    case 0:
+			    $where['_string']="substr(osn,1,6)<>'000000' and substr(osn,7,6)='000000'";
+			    break;
+		    case 1:
+			    $where['_string']="substr(osn,1,10)<>'000000' and substr(osn,11,2)='00'";
+			    break;
+	    }
+	    if(intval($level)<3){
+		    $sub_depts=$M_office->where($where)->field(array('oid','oname','osimplename'))->select();
+		    $this->success($sub_depts);
+	    }else{
+		    $this->error("无可转发单位");
+	    }
+    }
+    public function forward(){
+	    if(IS_POST){
+		    $title=I('post.fd_title');
+		    $mailid=I('post.fd_mailid');
+		    $mailtype=2;
+		    $comment=I('post.fd_comment');
+		    $source=get_department_id();
+		    $target=I('post.fd_target');
+	    $info = $this->db->where(array('id' => $mailid))->find();
+	    if($info['deptid']!=get_department_id()){
+		$this->error("您无权操作该信件");
+		}
+		$this->db->startTrans();
+		$flag=1;
+		if($target){
+			$data['deptid']=$target;
+			$ret=$this->db->where(array('id'=>$mailid))->save($data);
+			if($ret===false){
+				$flag=0;		
+				$error = $this->db->getError();
+			}
+			$forward=array('mailid'=>$mailid
+					,'mailtype'=>$mailtype
+					,'from'=>$source
+					,'to'=>$target
+					,'comment'=>$comment
+					,'createtime'=>date("Y-m-d H:i:s")
+					);
+			$ret=D("comment")->add($forward);
+			if(!$ret){
+				$flag=0;		
+				$error=$M_comment->getError();
+			}
+			if(!$flag){
+				$this->db->rollback();
+				$this->error($error ? $error : '转发失败！');
+			}else{
+				$this->db->commit();
+				$this->success("信件转发成功",U('index'));
+			}
+		}else{
+		    $this->error("转发失败:获取目标部门(detp:$target)失败！");
+		}
+	    }
     }
 }
